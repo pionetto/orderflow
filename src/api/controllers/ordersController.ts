05344bc033e478391fbd0cd54f8e47ab58d9214c
Pio Lima <pio.lima@gmail.com>
2025-06-11 07:27:59 -0300

    fix: correcao de lint 2

--- src/api/controllers/ordersController.ts
+++ src/api/controllers/ordersController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { lastNormalized } from './uploadController';
-import { NormalizedUser, NormalizedOrder } from '../../types/NormalizedOrder';
+import { NormalizedOrder } from '../../types/NormalizedOrder';
 
 function isValidDate(date: string): boolean {
   return /^\d{4}-\d{2}-\d{2}$/.test(date) && !isNaN(new Date(date).getTime());
