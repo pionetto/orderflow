7336abd0613c42f51f29418742e3265871e23c99
Pio Lima <pio.lima@gmail.com>
2025-06-11 01:34:48 -0300

    fix: corrige parser do valor

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -2,12 +2,14 @@ import { OrderLine } from '../types/OrderLine';
 
 export class OrderFileParser {
   static parseLine(line: string): OrderLine {
-    
     const userId = parseInt(line.slice(0, 10), 10);
     const userName = line.slice(10, 55).trim();
     const orderId = parseInt(line.slice(55, 65), 10);
     const productId = parseInt(line.slice(65, 75), 10);
-    const value = parseFloat(line.slice(75, 87).trim());
+
+    const rawValue = line.slice(75, 87);
+    const value = parseFloat(rawValue.replace(',', '.'));
+
     const date = line.slice(87, 95);
 
     return {
@@ -21,10 +23,9 @@ export class OrderFileParser {
   }
 
   static parseFileContent(content: string): OrderLine[] {
-    
     return content
       .split('\n')
-      .map(line => line.trim())
+      .map(line => line.trimEnd())
       .filter(line => line.length > 0)
       .map(OrderFileParser.parseLine);
   }
